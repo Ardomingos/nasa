dfc699ab53a52c9b2fcd1590af2c25c6aca158ee
André Rodrigues <rodrigues.domingos996@gmail.com>
2022-01-28 13:15:28 -0300

    criando a parte logica e adaptando o html

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-var botao = document.querySelector("#enviar");
+const url = "https://api.nasa.gov/planetary/apod?api_key=";
+var chave = "BNjUedFKICh225wd8VhQnuX2aZswmzREqqcCGBBO";
 
-botao.addEventListener("click", function(){
+var final = url + chave;
 
-    $("#enviar").
+window.onload = async function(){
 
-})
+    const response = await fetch(`${url}${chave}`);
+    const dados = await response.json();
+
+    var titulo = document.getElementById("titulo").innerHTML = dados.title;   
+    var explicacao = document.getElementById("explicacao").innerHTML = dados.explanation;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/*fetch(`${url}${chave}`)
+    .then(response => response.json())
+    .then(dados => {
+        const div = document.getElementById("conteudo");
+        div.innerHTML = dados.hdurl;
+    })
+    .catch(erro => console.log(erro));*/
+
